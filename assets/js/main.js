c7e47ec5ac6a4a77122ff55c857012a65a4b3dd2
Adrian <adriantorrescv@gmail.com>
2023-02-19 20:55:12 -0300

    Fix bug on main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -130,11 +130,7 @@
 					$('.main.style1')
 						.scrollex({
 							mode:		'middle',
-<<<<<<< HEAD
-							delay:		10,
-=======
 							delay:		50,
->>>>>>> d90139f1ef648c7ec48002308d757251faf77d63
 							initialize:	function() { $(this).addClass('inactive'); },
 							terminate:	function() { $(this).removeClass('inactive'); },
 							enter:		function() { $(this).removeClass('inactive'); },
@@ -144,11 +140,7 @@
 					$('.main.style2')
 						.scrollex({
 							mode:		'middle',
-<<<<<<< HEAD
-							delay:		10,
-=======
 							delay:		50,
->>>>>>> d90139f1ef648c7ec48002308d757251faf77d63
 							initialize:	function() { $(this).addClass('inactive'); },
 							terminate:	function() { $(this).removeClass('inactive'); },
 							enter:		function() { $(this).removeClass('inactive'); },
